7f697e960b079c85c56bbadbc2b711b43e23e518
Laith1305 <laithzca05@gmail.com>
2026-01-22 10:50:53 -0700

    Raycast Perception

--- Assets/Agent/Scripts/Perception.cs
+++ Assets/Agent/Scripts/Perception.cs
@@ -6,8 +6,11 @@ public abstract class Perception : MonoBehaviour
     [SerializeField] string info;
 
     [SerializeField] protected string tagName;
-    [SerializeField] protected float maxDistance;
+    [SerializeField] protected LayerMask layerMask = Physics.AllLayers;
+    [SerializeField] protected float maxDistance = 5;
     [SerializeField, Range(0, 180)] protected float maxAngle;
+
+
     public abstract GameObject[] GetGameObjects();
 
 
